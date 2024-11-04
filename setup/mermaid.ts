d621e45c22f9a49fafa5794f26061eb3741b05ab
beer-psi <lacvtg.a1.2023@gmail.com>
2024-11-04 08:11:36 +0700

    fix: screw the custom font

--- setup/mermaid.ts
+++ setup/mermaid.ts
@@ -2,10 +2,6 @@ import { defineMermaidSetup } from "@slidev/types";
 
 export default defineMermaidSetup(() => ({
     theme: "dark",
-    themeVariables: {
-        fontFamily: "Nunito Sans",
-        fontSize: "14px",
-    },
     class: {
         hideEmptyMembersBox: true,
     },
